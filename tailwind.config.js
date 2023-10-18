15fb2f7ccf5c02225398f9da7eeb9a4472181105
Jaber <89610911+AmirNajjari@users.noreply.github.com>
2023-10-18 16:51:07 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        ArchimotoV00: ["ArchimotoV00"],
+        TitanOne: ["Titan One"],
+      },
+    },
+  },
+  plugins: [],
+};
